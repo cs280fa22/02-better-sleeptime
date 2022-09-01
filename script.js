fad4fc88855191a3ba7e04ba49d7481c3f196de2
Ali Madooei <alimadooei@gmail.com>
2022-09-01 11:18:39 -0400

    Use a different color for each wakeup time.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const colors = [
+  "rgb(168, 39, 254)",
+  "rgb(154, 3, 254)",
+  "rgb(150, 105, 254)",
+  "rgb(140, 140, 255)",
+  "rgb(187, 187, 255)",
+  "rgb(143, 254, 221)",
+]
+
+function createWakeUpTimeElement(time, cycle) {
+  let elm = document.createElement("div");
+  elm.innerText = time;
+  elm.style.color = colors[cycle];
+  return elm;
+}
+
 function handleOnClick() {
   let output = document.querySelector(".output");
   output.style.display = "block";
@@ -12,6 +28,10 @@ function handleOnClick() {
   // calculate sleep cycles!
   for (let i = 1; i <= 6; i++) {
     now.setMinutes(now.getMinutes() + 90);
-    hours.innerHTML += now.toLocaleTimeString("en-US", { timeStyle: "short" }) + "<br />";
+    const elm = createWakeUpTimeElement(
+      now.toLocaleTimeString("en-US", { timeStyle: "short" }),
+      i
+    )
+    hours.appendChild(elm);
   } 
 }
